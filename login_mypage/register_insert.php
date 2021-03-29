19f38fb21ec26f55c89ab66434b36df3e2ba4fe2
YUKIWEB888 <yukiweb888@gmail.com>
2021-03-29 17:06:30 +0900

    マイページ昨日

---
+++ login_mypage/register_insert.php
@@ -0,0 +1,28 @@
+﻿<?php
+mb_internal_encoding("utf8");
+
+try{
+//DB接続
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+}catch(PDOException $e){
+    die("<p>申し訳ございません。現在サーバーが込み合っており一時的にアクセスが出来ません。<br>しばらくしてから再度ログインをしてください。</p>
+    <a href='http://localhoct/login_mypage/login.php'>ログイン画面へ</a>"
+    );
+}
+//prepared statement（プリペアードステートメント）でSQL文の型を作る
+
+$stmt = $pdo->prepare("insert into login_mypage(name,mail,password,picture,comments)values(?,?,?,?,?)");
+
+//bindValueメソッドでパラメータをセット
+$stmt->bindValue(1,$_POST['name']);
+$stmt->bindValue(2,$_POST['mail']);
+$stmt->bindValue(3,$_POST['password']);
+$stmt->bindValue(4,$_POST['path_filename']);
+$stmt->bindValue(5,$_POST['comments']);
+
+//executeでクエリを実行。
+$stmt->execute();
+$pdo = NULL;
+
+header('Location:after_register.html');
+?>
